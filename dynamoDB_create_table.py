538bb4ec0ae4ad9c3cac14300a82e9495a6ce7de
Sandun Chandrasiri <131155243+sandun-chandrasiri@users.noreply.github.com>
2023-06-26 13:29:56 +0530

    Add files via upload

---
+++ dynamoDB_create_table.py
@@ -0,0 +1,46 @@
+"""
+Created on Tue June 07 2023
+
+@author: NirvanaClouds Academy ~ Dasun Athukorala
+"""
+
+import boto3
+
+import key_config as keys
+
+
+dynamodb_client = boto3.client(
+    'dynamodb',
+    #aws_access_key_id     = keys.ACCESS_KEY_ID,
+    #aws_secret_access_key = keys.ACCESS_SECRET_KEY,
+    region_name           = keys.REGION_NAME,
+)
+dynamodb_resource = boto3.resource(
+    'dynamodb',
+    #aws_access_key_id     = keys.ACCESS_KEY_ID,
+    #aws_secret_access_key = keys.ACCESS_SECRET_KEY,
+    region_name           = keys.REGION_NAME,
+)
+
+
+def create_table():
+   table = dynamodb_resource.create_table(
+       TableName = 'users', # Name of the table
+       KeySchema = [
+           {
+               'AttributeName': 'email',
+               'KeyType'      : 'HASH' #RANGE = sort key, HASH = partition key
+           }
+       ],
+       AttributeDefinitions = [
+           {
+               'AttributeName': 'email', # Name of the attribute
+               'AttributeType': 'S'   # N = Number (B= Binary, S = String)
+           }
+       ],
+       ProvisionedThroughput={
+           'ReadCapacityUnits'  : 10,
+           'WriteCapacityUnits': 10
+       }
+   )
+   return table
---
+++ key_config.py
@@ -0,0 +1,13 @@
+"""
+Created on Tue June 07 2023
+
+@author: NirvanaClouds Academy ~ Dasun Athukorala
+"""
+
+
+ACCESS_KEY_ID='ASIBC3FEP6WZKBLO6J23DG'
+ACCESS_SECRET_KEY='/YSOI5dST5oC4fpIOagd678bc8299E6Y3rK4bgbci'
+REGION_NAME='ap-northeast-1'
+
+
+#BUCKET_NAME='unigrade-test-bucket-test'
